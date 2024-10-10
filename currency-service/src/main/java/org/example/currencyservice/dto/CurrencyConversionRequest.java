5990734ec26ef49d83d3c44cd1c4e43196373bdb
William <arsenovova7@gmail.com>
2024-10-10 18:05:13 +0300

    Add currency conversion test

--- currency-service/src/main/java/org/example/currencyservice/dto/CurrencyConversionRequest.java
+++ currency-service/src/main/java/org/example/currencyservice/dto/CurrencyConversionRequest.java
@@ -3,8 +3,10 @@ package org.example.currencyservice.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class CurrencyConversionRequest {
     @NotBlank(message = "fromCurrency is mandatory")
