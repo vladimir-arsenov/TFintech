fec3414de09e628d4b117165d15b751fff75214b
William <arsenovova7@gmail.com>
2024-10-31 18:33:59 +0300

    move eventSpecification() to service

--- src/main/java/org/example/repository/EventRepository.java
+++ src/main/java/org/example/repository/EventRepository.java
@@ -5,37 +5,10 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findAll(Specification<Event> spec);
-
-
-    static Specification<Event> eventSpecification(String name, Long location, LocalDate fromDate, LocalDate toDate) {
-        List<Specification<Event>> specs = new ArrayList<>();
-        if (name != null) {
-            specs.add((event, query, cb) -> cb.equal(event.get("name"), name));
-        }
-
-        if (location != null) {
-            specs.add((event, query, cb) -> cb.equal(event.get("location").get("id"), location));
-        }
-
-        if (fromDate != null && toDate != null) {
-            specs.add((event, query, cb) -> cb.between(event.get("date"), fromDate, toDate));
-        } else if (fromDate != null) {
-            specs.add((event, query, cb) -> {
-                System.out.println(event.get("date") + " " + fromDate);
-                return cb.greaterThanOrEqualTo(event.get("date"), fromDate);});
-        } else if (toDate != null) {
-            specs.add((event, query, cb) -> cb.lessThanOrEqualTo(event.get("date"), toDate));
-        }
-
-        return specs.stream().reduce(Specification::and).orElse(null);
-
-    }
 }
--- src/main/java/org/example/service/EventService.java
+++ src/main/java/org/example/service/EventService.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.example.model.Event;
 import org.example.model.Location;
 import org.example.repository.EventRepository;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,7 +20,7 @@ public class EventService {
     private final LocationService locationService;
 
     public List<Event> getAll(String name, Long location, LocalDate fromDate, LocalDate toDate) {
-        return eventRepository.findAll(EventRepository.eventSpecification(name, location, fromDate, toDate))
+        return eventRepository.findAll(eventSpecification(name, location, fromDate, toDate))
                 .stream()
                 .toList();
     }
@@ -47,5 +49,26 @@ public class EventService {
 
     public void delete(Long id) {
         eventRepository.delete(get(id));
+    }
+
+    private Specification<Event> eventSpecification(String name, Long location, LocalDate fromDate, LocalDate toDate) {
+        List<Specification<Event>> specs = new ArrayList<>();
+        if (name != null) {
+            specs.add((event, query, cb) -> cb.equal(event.get("name"), name));
+        }
+
+        if (location != null) {
+            specs.add((event, query, cb) -> cb.equal(event.get("location").get("id"), location));
+        }
+
+        if (fromDate != null && toDate != null) {
+            specs.add((event, query, cb) -> cb.between(event.get("date"), fromDate, toDate));
+        } else if (fromDate != null) {
+            specs.add((event, query, cb) -> cb.greaterThanOrEqualTo(event.get("date"), fromDate));
+        } else if (toDate != null) {
+            specs.add((event, query, cb) -> cb.lessThanOrEqualTo(event.get("date"), toDate));
+        }
+
+        return specs.stream().reduce(Specification::and).orElse(null);
     }
 }
