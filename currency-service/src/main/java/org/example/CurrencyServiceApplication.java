47b0da7977a46fa76df49ac1f43f1d3614dffc60
vladimir-arsenov <arsenovova7@gmail.com>
2024-11-07 21:25:04 +0300

    add modules

--- currency-service/src/main/java/org/example/CurrencyServiceApplication.java
+++ currency-service/src/main/java/org/example/CurrencyServiceApplication.java
@@ -1,4 +1,4 @@
-package org.example.currencyservice;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
 rootProject.name = "TFintech"
 include("execution-time-logger-starter")
 include("event-location-category-service")
+include("currency-service")
+include("custom-linked-list")
