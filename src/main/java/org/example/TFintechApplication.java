fcbac9f68d133801b7db65379cf42e7f59d88eef
William <arsenovova7@gmail.com>
2024-11-07 19:07:49 +0300

    revert to fec3414de09e628d4b117165d15b751fff75214b

--- src/main/java/org/example/TFintechApplication.java
+++ src/main/java/org/example/TFintechApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TFintechApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(TFintechApplication.class, args);
     }
 }
